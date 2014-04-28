430a123304574014f55adb5ed7ef1646e80ce6ce
sanghavimayur13 <silentsoul_2007@yahoo.com>
2014-04-27 22:23:25 -0400

    Project Created

---
+++ plot.py
@@ -0,0 +1,29 @@
+import sys
+import csv
+import math
+import matplotlib.pylab as plt
+import numpy as np
+
+
+def main():
+	file1 = open(sys.argv[1])
+	str = csv.reader(file1)
+	for s in str:
+		x = float(s[0])
+		y = float(s[1])
+		c = float(s[2])
+		if(c == 1):
+			plt.plot(x,y,'rs')
+		elif(c == 2):
+			plt.plot(x, y, 'bo')
+		elif(c == 3):
+			plt.plot(x, y, 'go')
+		else:
+			plt.plot(x, y, 'k^')
+		plt.xlabel("Rot.")
+		plt.ylabel("Ecc")
+	plt.show()
+		
+
+if __name__ == '__main__':
+	main()
